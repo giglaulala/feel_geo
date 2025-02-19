bc7258e968debd67a3a889bbaaaa296d185266f9
giglaulala <gigla.titina777@gmail.com>
2025-02-19 19:37:42 +0400

    feat: add about page

--- feel-geo-ge/src/components/About.jsx
+++ feel-geo-ge/src/components/About.jsx
@@ -1,7 +1,23 @@
+import { styles } from "./About.module.css";
+
 function About() {
   return (
     <div>
-      <h1>ჩვენს შესახებ</h1>
+      <h1 className={styles.head}>ჩვენს შესახებ</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        cupiditate quis id enim, iusto ea ipsa, impedit rerum vel modi magnam
+        voluptatem, officiis eos eveniet a numquam. Voluptas, placeat nostrum.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis corrupti
+        qui dignissimos numquam, ut sunt illo veniam autem voluptatem, rem
+        debitis fugiat dolores veritatis inventore ducimus error quas aliquam
+        in. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem et
+        earum, aut veniam debitis harum quis quas rerum eius fugit ullam vero
+        dolor id, animi eos cupiditate? Non, iusto ex. Lorem ipsum dolor sit
+        amet, consectetur adipisicing elit. Aliquid quas exercitationem
+        laboriosam molestias labore ex, id quod at autem? Harum recusandae
+        tempore ad debitis est quas laboriosam eveniet incidunt aut.
+      </div>
     </div>
   );
 }
