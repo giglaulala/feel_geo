bb4b109785627b4a96ec8630c321dfbc6f0a0636
giglaulala <gigla.titina777@gmail.com>
2025-02-19 19:02:34 +0400

    feat: add react router fully

--- feel-geo-ge/src/App.jsx
+++ feel-geo-ge/src/App.jsx
@@ -4,8 +4,6 @@ import Main from "./components/Main.jsx";
 import About from "./components/About.jsx";
 import Services from "./components/Services.jsx";
 import Contact from "./components/Contact.jsx";
-
-import SubHeader from "./components/SubHeader.jsx";
 import { Routes, Route } from "react-router-dom";
 import "./App.module.css";
 
@@ -14,13 +12,11 @@ function App() {
     <div>
       <Header />
       <Routes>
-        {/* <Route path="/" element={<Main />} /> */}
+        <Route path="/" element={<Main />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <SubHeader className="sub" />
-      <Main className="main" />
       <Footer className="footer" />
     </div>
   );
--- feel-geo-ge/src/components/Main.jsx
+++ feel-geo-ge/src/components/Main.jsx
@@ -1,70 +1,75 @@
 import Card from "./Card.jsx";
 import styles from "./Main.module.css";
+import SubHeader from "./SubHeader.jsx";
 
 function Main() {
   return (
-    <div className={styles.mainDiv}>
-      <Card
-        img={"sea-7375377_1280.jpg"}
-        name={"Tour"}
-        description={"Travel around the world"}
-        price={500}
-      />
-      <Card
-        img={"sea-7375377_1280.jpg"}
-        name={"Tour"}
-        description={"Travel around the world"}
-        price={500}
-      />
-      <Card
-        img={"sea-7375377_1280.jpg"}
-        name={"Tour"}
-        description={"Travel around the world"}
-        price={500}
-      />
-      <Card
-        img={"sea-7375377_1280.jpg"}
-        name={"Tour"}
-        description={"Travel around the world"}
-        price={500}
-      />
-      <Card
-        img={"sea-7375377_1280.jpg"}
-        name={"Tour"}
-        description={"Travel around the world"}
-        price={500}
-      />
-      <Card
-        img={"sea-7375377_1280.jpg"}
-        name={"Tour"}
-        description={"Travel around the world"}
-        price={500}
-      />
-      <Card
-        img={"sea-7375377_1280.jpg"}
-        name={"Tour"}
-        description={"Travel around the world"}
-        price={500}
-      />
-      <Card
-        img={"sea-7375377_1280.jpg"}
-        name={"Tour"}
-        description={"Travel around the world"}
-        price={500}
-      />
-      <Card
-        img={"sea-7375377_1280.jpg"}
-        name={"Tour"}
-        description={"Travel around the world"}
-        price={500}
-      />
-      <Card
-        img={"sea-7375377_1280.jpg"}
-        name={"Tour"}
-        description={"Travel around the world"}
-        price={500}
-      />
-    </div>
+    <>
+      <SubHeader />
+
+      <div className={styles.mainDiv}>
+        <Card
+          img={"sea-7375377_1280.jpg"}
+          name={"Tour"}
+          description={"Travel around the world"}
+          price={500}
+        />
+        <Card
+          img={"sea-7375377_1280.jpg"}
+          name={"Tour"}
+          description={"Travel around the world"}
+          price={500}
+        />
+        <Card
+          img={"sea-7375377_1280.jpg"}
+          name={"Tour"}
+          description={"Travel around the world"}
+          price={500}
+        />
+        <Card
+          img={"sea-7375377_1280.jpg"}
+          name={"Tour"}
+          description={"Travel around the world"}
+          price={500}
+        />
+        <Card
+          img={"sea-7375377_1280.jpg"}
+          name={"Tour"}
+          description={"Travel around the world"}
+          price={500}
+        />
+        <Card
+          img={"sea-7375377_1280.jpg"}
+          name={"Tour"}
+          description={"Travel around the world"}
+          price={500}
+        />
+        <Card
+          img={"sea-7375377_1280.jpg"}
+          name={"Tour"}
+          description={"Travel around the world"}
+          price={500}
+        />
+        <Card
+          img={"sea-7375377_1280.jpg"}
+          name={"Tour"}
+          description={"Travel around the world"}
+          price={500}
+        />
+        <Card
+          img={"sea-7375377_1280.jpg"}
+          name={"Tour"}
+          description={"Travel around the world"}
+          price={500}
+        />
+        <Card
+          img={"sea-7375377_1280.jpg"}
+          name={"Tour"}
+          description={"Travel around the world"}
+          price={500}
+        />
+      </div>
+    </>
   );
 }
 
