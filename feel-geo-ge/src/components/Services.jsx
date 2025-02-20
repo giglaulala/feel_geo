a9510e2a24d7380df1f85c30e48f8cf2bcca58a5
giglaulala <gigla.titina777@gmail.com>
2025-02-20 16:36:30 +0400

    feat:add title img, services

--- feel-geo-ge/src/components/Services.jsx
+++ feel-geo-ge/src/components/Services.jsx
@@ -1,5 +1,71 @@
+import Card from "./Card.jsx";
+import styles from "./Services.module.css";
+
 function Services() {
-  return <div>Services</div>;
+  return (
+    <div className={styles.mainDiv}>
+      <Card
+        img={"sea-7375377_1280.jpg"}
+        name={"Tour"}
+        description={"Travel around the world"}
+        price={500}
+      />
+      <Card
+        img={"sea-7375377_1280.jpg"}
+        name={"Tour"}
+        description={"Travel around the world"}
+        price={500}
+      />
+      <Card
+        img={"sea-7375377_1280.jpg"}
+        name={"Tour"}
+        description={"Travel around the world"}
+        price={500}
+      />
+      <Card
+        img={"sea-7375377_1280.jpg"}
+        name={"Tour"}
+        description={"Travel around the world"}
+        price={500}
+      />
+      <Card
+        img={"sea-7375377_1280.jpg"}
+        name={"Tour"}
+        description={"Travel around the world"}
+        price={500}
+      />
+      <Card
+        img={"sea-7375377_1280.jpg"}
+        name={"Tour"}
+        description={"Travel around the world"}
+        price={500}
+      />
+      <Card
+        img={"sea-7375377_1280.jpg"}
+        name={"Tour"}
+        description={"Travel around the world"}
+        price={500}
+      />
+      <Card
+        img={"sea-7375377_1280.jpg"}
+        name={"Tour"}
+        description={"Travel around the world"}
+        price={500}
+      />
+      <Card
+        img={"sea-7375377_1280.jpg"}
+        name={"Tour"}
+        description={"Travel around the world"}
+        price={500}
+      />
+      <Card
+        img={"sea-7375377_1280.jpg"}
+        name={"Tour"}
+        description={"Travel around the world"}
+        price={500}
+      />
+    </div>
+  );
 }
 
 export default Services;
