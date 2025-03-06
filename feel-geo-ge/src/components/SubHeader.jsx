26434f533bc8d6584d0f6d79567cc8bb9c7898f3
giglaulala <gigla.titina777@gmail.com>
2025-03-06 20:09:39 +0400

    feat: add time interaction

--- feel-geo-ge/src/components/SubHeader.jsx
+++ feel-geo-ge/src/components/SubHeader.jsx
@@ -1,8 +1,23 @@
 import styles from "./SubHeader.module.css";
+import { useState, useEffect } from "react";
 
 function SubHeader() {
+  const [timePicture, setTimePicture] = useState(null);
+
+  useEffect(() => {
+    const date = new Date();
+    setTimePicture(date.getHours());
+  }, []);
+  console.log(timePicture);
   return (
-    <div className={styles.mainDiv}>
+    <div
+      style={
+        timePicture > 19 || timePicture < 8
+          ? { backgroundImage: `url("/tbilisi-at-night.jpeg")` }
+          : { backgroundImage: `url("/tbilisi.jpg")` }
+      }
+      className={styles.mainDiv}
+    >
       <div className={styles.mainText}>Feel Geo</div>
       <div className={styles.subText}>with George</div>
     </div>
